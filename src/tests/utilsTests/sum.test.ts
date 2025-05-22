3c04cb5a344a8fc319251496accb5f17c7d1cf94
ArsagaKamil <161819048+ArsagaKamil@users.noreply.github.com>
2025-05-22 12:02:30 +0900

    write custom test for sum

---
+++ src/tests/utilsTests/sum.test.ts
@@ -0,0 +1,8 @@
+import { expect, describe, it } from "vitest";
+import { sum } from "../../utils/sum";
+
+describe("sum test", () => {
+  it("should return 3 when 1 and 2 are passed", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
