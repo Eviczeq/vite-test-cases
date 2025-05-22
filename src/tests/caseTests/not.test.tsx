919071449d7dcbc03e3a1a3a34bfeed3c1b9d52d
ArsagaKamil <161819048+ArsagaKamil@users.noreply.github.com>
2025-05-22 12:12:31 +0900

    add not test case

--- src/tests/caseTests/not.test.tsx
+++ src/tests/caseTests/not.test.tsx
@@ -1,11 +1,10 @@
 import { expect, describe, test } from "vitest";
-
 const sum = (a: number, b: number) => a + b;
 
-describe("toBe test", () => {
-  test("1+2は3になる", () => {
+describe("not test", () => {
+  test("1+2は5ではない", () => {
     const a = 1;
     const b = 2;
-    expect(sum(a, b)).toBe(3);
+    expect(sum(a, b)).not.toBe(5);
   });
 });
--- src/tests/caseTests/toEqual.test.tsx
+++ src/tests/caseTests/toEqual.test.tsx
@@ -1,15 +1,15 @@
 import { expect, describe, test } from "vitest";
 
 describe("toEqual test", () => {
-  const data = [
-    { name: "John", age: 30 },
-    { name: "Jane", age: 25 },
-  ];
-  const target = {
-    name: "John",
-    age: 30,
-  };
   test("選択されたデータが期待値通りか", () => {
+    const data = [
+      { name: "John", age: 30 },
+      { name: "Jane", age: 25 },
+    ];
+    const target = {
+      name: "John",
+      age: 30,
+    };
     expect(data[0]).toEqual(target);
   });
 });
