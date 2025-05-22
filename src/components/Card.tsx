61e57b6ee58b2dab54f848812bb97ff8e850d8b5
ArsagaKamil <161819048+ArsagaKamil@users.noreply.github.com>
2025-05-22 18:29:38 +0900

    add cardTest

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,19 @@ type Props = {
   title: string;
   description: string;
   imageUrl: string;
+  onClick: () => void;
+  disabled?: boolean;
 };
 
 const ComponentName = (props: Props) => {
   return (
     <div>
-      <div>{props.title}</div>
+      <h1>{props.title}</h1>
       <img src={props.imageUrl} />
       <p>{props.description}</p>
+      <button disabled={props.disabled} onClick={props.onClick}>
+        Click me
+      </button>
     </div>
   );
 };
---
+++ src/tests/componentTests/card.test.tsx
@@ -0,0 +1,55 @@
+import { expect, describe, test, vi } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Card from "../../components/Card";
+
+describe("CardComponent test", () => {
+  const props = {
+    title: "title",
+    description: "description",
+    imageUrl: "imageUrl",
+    onClick: () => {
+      console.log("clicked");
+    },
+  };
+  test("titleがレンダリングできるか", () => {
+    render(<Card {...props} />);
+    expect(screen.getByText("title")).toBeDefined();
+  });
+
+  test("descriptionがレンダリングできるか", () => {
+    render(<Card {...props} />);
+    expect(screen.getByText("description")).toBeDefined();
+  });
+
+  test("imageUrlがレンダリングできるか", () => {
+    render(<Card {...props} />);
+    expect(screen.getByRole("img")).toBeDefined();
+  });
+
+  test("buttonがレンダリングできるか", () => {
+    render(<Card {...props} />);
+    expect(screen.getByRole("button")).toBeDefined();
+  });
+
+  test("buttonがクリックできるか", async () => {
+    const onClick = vi.fn();
+
+    render(<Card {...props} onClick={onClick} />);
+
+    const button = screen.getByRole("button");
+
+    await fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("disabledをtureにするとボタンがクリック出来なくなること", async () => {
+    const onClick = vi.fn();
+
+    render(<Card {...props} disabled={true} />);
+
+    const button = screen.getByRole("button");
+
+    await fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(0);
+  });
+});
